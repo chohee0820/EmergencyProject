14a7b0993cae727fed9480676e909a095a17b1c1
KnyChe0705 <chohee200820@gmail.com>
2024-04-01 19:56:36 +0900

    test

---
+++ EmergencyPJ01/src/main/java/com/emergency/EmergencyPj01Application.java
@@ -0,0 +1,13 @@
+package com.emergency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyPj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyPj01Application.class, args);
+	}
+
+}
